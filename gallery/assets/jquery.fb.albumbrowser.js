27b1cde9b2446f41c7dd0baa0de31c4d375d855b
reyesj5 <33083881+reyesj5@users.noreply.github.com>
2018-02-12 23:05:08 -0600

    fixed gallery styles

--- gallery/assets/jquery.fb.albumbrowser.js
+++ gallery/assets/jquery.fb.albumbrowser.js
@@ -201,7 +201,7 @@
                                             alt: "Back",
                                             height: 32,
                                             width: 32,
-                                            src: 'back.png',
+                                            src: 'img/back.png',
                                             class: "fb-albums-list"
                                         }));
 
@@ -607,8 +607,8 @@
                     if (settings.showImageText || settings.likeButton || settings.shareButton) {
                         lightboxContent.append($("<div>", { class: "fb-preview-text" }));
                     }
-                    overlay.append($("<img>", { class: "fb-preview-img-prev", src: "previous.png", alt: "prev", width: "30px" }));
-                    overlay.append($("<img>", { class: "fb-preview-img-next", src: "next.png", alt: "next", width: "30px" }));
+                    overlay.append($("<img>", { class: "fb-preview-img-prev", src: "img/previous.png", alt: "prev", width: "30px" }));
+                    overlay.append($("<img>", { class: "fb-preview-img-next", src: "img/next.png", alt: "next", width: "30px" }));
 
                     $("body").append(overlay);
                     overlay = $(".fb-preview-overlay");
